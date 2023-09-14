f344e52a3c39d46c5cba670f6283b00eab2145f5
felipricardo <felipebarbosaok@gmail.com>
2023-09-14 13:29:34 -0300

    up

---
+++ Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IWantApp.Domain.Products;
+
+using IWantApp.Infra.Data;
+
+
+// Classe de contexto de banco de dados
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+    }
+}
