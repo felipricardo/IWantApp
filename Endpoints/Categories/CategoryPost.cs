dddba97e01c3bc05e5fd70c68f57699ce37fa10f
felipricardo <felipebarbosaok@gmail.com>
2023-09-14 14:04:47 -0300

    up

--- Endpoints/Categories/CategoryPost.cs
+++ Endpoints/Categories/CategoryPost.cs
@@ -4,11 +4,11 @@ namespace IWantApp.Endpoints.Categories;
 
 public class CategoryPost
 {
-    public string Template => "/categories";
-    public string[] Methods => new string[] { HttpMethod.Post.ToString() };
-    public Delegate Handle => Action;
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
 
-    public IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+    public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
     {
         return Results.Ok("Ok");
     }
