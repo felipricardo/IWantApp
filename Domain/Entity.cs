42738897e0b5e723b61d30497605872b47e082bb
felipricardo <felipebarbosaok@gmail.com>
2023-09-14 12:02:48 -0300

    up

---
+++ Domain/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+    
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string CreateBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditeBy { get; set; }
+    public DateTime EditeOn { get; set; }
+}
--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -1,14 +1,9 @@
 ﻿namespace IWantApp.Domain.Products;
 
-public class Product
+public class Product : Entity
 {
-    public Guid Id { get; set; }
     public string Name { get; set; }
     public Category Category { get; set; }
     public string Description { get; set; }
     public bool HasStock { get; set; }
-    public string CreateBy { get; set; }
-    public DateTime CreatedOn { get; set; }
-    public string EditeBy { get; set; }
-    public DateTime EditeOn { get; set; }
 }
