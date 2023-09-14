de59ccc2aead35b1c1b39a474b44a14b68145978
felipricardo <felipebarbosaok@gmail.com>
2023-09-14 11:52:14 -0300

    up

---
+++ Domain/Products/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string CreateBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditeBy { get; set; }
+    public DateTime EditeOn { get; set; }
+}
