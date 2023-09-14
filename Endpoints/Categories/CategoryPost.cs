b30573ccc5ef17b62339d3b8e3a88b71a576c259
felipricardo <felipebarbosaok@gmail.com>
2023-09-14 14:03:41 -0300

    up

---
+++ Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,16 @@
+﻿using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPost
+{
+    public string Template => "/categories";
+    public string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public Delegate Handle => Action;
+
+    public IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+        return Results.Ok("Ok");
+    }
+
+}
