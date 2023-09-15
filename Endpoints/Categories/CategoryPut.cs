a2003ba94bacb5892490e8e11c3fbb2b0aed1a6d
felipricardo <felipebarbosaok@gmail.com>
2023-09-15 14:52:14 -0300

    up

--- Endpoints/Categories/CategoryPut.cs
+++ Endpoints/Categories/CategoryPut.cs
@@ -6,21 +6,16 @@ namespace IWantApp.Endpoints.Categories;
 
 public class CategoryPut
 {
-    public static string Template => "/categories";
+    public static string Template => "/categories/{id}";
     public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
     public static Delegate Handle => Action;
 
     public static IResult Action([FromRoute] Guid id, CategoryRequest categoryRequest, ApplicationDbContext context)
     {
-        var category = new Category
-        {
-            Name = categoryRequest.Name,
-            CreateBy = "Test",
-            CreatedOn = DateTime.Now,
-            EditeBy = "Test",
-            EditeOn = DateTime.Now
-        };
-        context.Categories.Add(category);
+        var category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
+        category.Name = categoryRequest.Name;
+        category.Active = categoryRequest.Active;
+
         context.SaveChanges();
 
         return Results.Created($"/categories/{category.Id}", category.Id);
