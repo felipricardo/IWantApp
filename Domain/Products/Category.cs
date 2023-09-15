77bcc543b118aa35f6ea6e01aa7a3f3ba75fbf2b
felipricardo <felipebarbosaok@gmail.com>
2023-09-15 15:44:56 -0300

    up

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -10,7 +10,7 @@ public class Category : Entity
     public Category(string name)
     {
         var contract = new Contract<Category>()
-            .IsNotNull(name, "Name");
+            .IsNotNullOrEmpty(name, "Name");
         AddNotifications(contract);
 
         Name = name;
