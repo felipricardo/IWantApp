2957797fa72cc3d11e5e728ac6fbdd808894cad6
felipricardo <felipebarbosaok@gmail.com>
2023-09-18 16:14:22 -0300

    up

--- Endpoints/ProblemDetailsExtensions.cs
+++ Endpoints/ProblemDetailsExtensions.cs
@@ -12,10 +12,10 @@ public static class ProblemDetailsExtensions
                 .ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
     }
 
-    public static Dictionary<string, string[]> ConvertToProblemDetails(this IReadOnlyCollection<IdentityError> error)
+    public static Dictionary<string, string[]> ConvertToProblemDetails(this IEnumerable<IdentityError> error)
     {
-        return error
-                .GroupBy(g => g.Code)
-                .ToDictionary(g => g.Key, g => g.Select(x => x.Description).ToArray());
+        var dictionary = new Dictionary<string, string[]>();
+        dictionary.Add("Error", error.Select(e => e.Description).ToArray());
+        return dictionary;
     }
 }
