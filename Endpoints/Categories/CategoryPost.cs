1d6ecf7b5b30a0a007288f8a01f61641b93f5db0
felipricardo <felipebarbosaok@gmail.com>
2023-09-18 09:15:38 -0300

    up

--- Endpoints/Categories/CategoryPost.cs
+++ Endpoints/Categories/CategoryPost.cs
@@ -14,7 +14,10 @@ public class CategoryPost
         var category = new Category(categoryRequest.Name, "Test", "Test");
 
         if (!category.IsValid)
-            return Results.BadRequest(category.Notifications);
+        {
+            var erros = category.Notifications.GroupBy(g => g.Key).ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
+            return Results.ValidationProblem(erros);
+        }
         
         context.Categories.Add(category);
         context.SaveChanges();
