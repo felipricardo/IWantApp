a4a9ce4a4082d83e809bfc50f78b1844a187ea1f
felipricardo <felipebarbosaok@gmail.com>
2023-09-18 16:28:56 -0300

    up

--- Endpoints/Employees/EmployeeGetAll.cs
+++ Endpoints/Employees/EmployeeGetAll.cs
@@ -9,7 +9,7 @@ public class EmployeeGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(UserManager<IdentityUser> userManager)
+    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
     {
         var users = userManager.Users.ToList();
         var employees = new List<EmployeeResponse>();
