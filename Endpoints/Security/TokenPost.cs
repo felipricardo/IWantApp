8d2a55a18df64ad9450c47d32be4292b42c34b31
felipricardo <felipebarbosaok@gmail.com>
2023-09-19 16:26:27 -0300

    up

--- Endpoints/Security/TokenPost.cs
+++ Endpoints/Security/TokenPost.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
-using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using System.Xml.Linq;
 
 namespace IWantApp.Endpoints.Security;
 
