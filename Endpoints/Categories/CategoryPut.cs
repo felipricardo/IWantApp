d152da78a4cf4b6cfaa5cf8c131999776c6fd056
felipricardo <felipebarbosaok@gmail.com>
2023-09-20 09:24:31 -0300

    up

--- Endpoints/Categories/CategoryPut.cs
+++ Endpoints/Categories/CategoryPut.cs
@@ -1,38 +1,47 @@
-﻿using IWantApp.Domain.Products;
-using IWantApp.Infra.Data;
+﻿using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Linq;
-using static System.Net.WebRequestMethods;
 using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Categories;
 
 public class CategoryPut
 {
+    // Define o template da URL para este endpoint
     public static string Template => "/categories/{id:guid}";
+
+    // Define os métodos HTTP permitidos para este endpoint
     public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
+
+    // Define o delegado que manipula a ação deste endpoint
     public static Delegate Handle => Action;
 
+    // Atributo para autorização com base em uma política específica
     [Authorize(Policy = "EmployeePolicy")]
     public static IResult Action(
         [FromRoute] Guid id, HttpContext http, CategoryRequest categoryRequest, ApplicationDbContext context)
     {
+        // Obtém o ID do usuário atual
         var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
+        // Busca a categoria pelo ID fornecido
         var category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
 
+        // Verifica se a categoria existe
         if (category == null)
             return Results.NotFound();
 
+        // Edita as informações da categoria
         category.EditInfo(categoryRequest.Name, categoryRequest.Active, userId);
 
+        // Verifica se a categoria é válida após a edição
         if (!category.IsValid)
             return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
 
+        // Salva as alterações no banco de dados
         context.SaveChanges();
 
+        // Retorna uma resposta HTTP 200 OK
         return Results.Ok();
     }
-
 }
--- Endpoints/Security/TokenPost.cs
+++ Endpoints/Security/TokenPost.cs
@@ -16,12 +16,14 @@ public class TokenPost
     [AllowAnonymous]
     public static IResult Action(LoginRequest loginRequest, IConfiguration configuration, UserManager<IdentityUser> userManager)
     {
+        // Encontra o usuário pelo e-mail
         var user = userManager.FindByEmailAsync(loginRequest.Email).Result;
-        if (user == null)
-            Results.BadRequest();
-        if (!userManager.CheckPasswordAsync(user, loginRequest.Password).Result)
-            Results.BadRequest();
 
+        // Verifica se o usuário existe e se a senha está correta
+        if (user == null || !userManager.CheckPasswordAsync(user, loginRequest.Password).Result)
+            return Results.BadRequest();
+
+        // Cria e configura o token JWT
         var claims = userManager.GetClaimsAsync(user).Result;
         var subject = new ClaimsIdentity(new Claim[]
         {
@@ -32,11 +34,9 @@ public class TokenPost
 
         var key = Encoding.ASCII.GetBytes(configuration["JwtBearerTokenSettings:SecretKey"]);
         var tokenDescriptor = new SecurityTokenDescriptor
-        {   
+        {
             Subject = subject,
-            SigningCredentials =
-                new SigningCredentials(
-                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
             Audience = configuration["JwtBearerTokenSettings:Audience"],
             Issuer = configuration["JwtBearerTokenSettings:Issuer"],
             Expires = DateTime.UtcNow.AddSeconds(30)
@@ -44,10 +44,11 @@ public class TokenPost
 
         var tokenHandler = new JwtSecurityTokenHandler();
         var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        // Retorna o token
         return Results.Ok(new
         {
             token = tokenHandler.WriteToken(token)
         });
     }
-
 }
